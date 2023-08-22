172f90475e9c8be7e090d8178d2997e2c713765f
Khausta <anna.a.khaustova@gmail.com>
2023-08-22 09:18:10 +0300

    hometask 5 is executed

---
+++ 5-languages.js
@@ -0,0 +1,12 @@
+const language = prompt('Enter your language', '');
+switch(language) {
+    case('en'):
+        console.log('Good afternoon!');
+        break;
+    case('de'):
+        console.log('Guten Tag!');
+        break;
+    case('ru'):
+        console.log('Добрый день');
+        break;
+}
