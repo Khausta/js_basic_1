ec0af359188bbcbb63574e0c76a1981a0d16b69b
Khausta <anna.a.khaustova@gmail.com>
2023-08-23 09:09:35 +0300

    thinking about logic of app

---
+++ 7-converter.js
@@ -0,0 +1,11 @@
+const sum = +prompt('Please enter your budget', '');
+const currency = prompt('Please enter your currency', '');
+const targetCurrency = prompt('Please enter the currency for convertation', '');
+
+function rublesToDollars() {
+    
+}
+
+function dollarsToRubles() {
+
+}
