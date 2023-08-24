e1e5a83c472ffd8892831d0febb377e35da55555
Khausta <anna.a.khaustova@gmail.com>
2023-08-24 08:59:51 +0300

    input check is added

--- 7-converter.js
+++ 7-converter.js
@@ -2,10 +2,31 @@ const sum = +prompt('Please enter your budget', '');
 const currency = prompt('Please enter your currency', '');
 const targetCurrency = prompt('Please enter the currency for convertation', '');
 
-function rublesToDollars() {
-    
+const bets = {
+    dollarToRubles: 94.44,
+    euroToRubles: 102.25,
+    CHFToRubles: 107.27,
 }
 
-function dollarsToRubles() {
+let dollarsToRubles = () => sum * bets.dollarToRubles;
+let rublesToDollars = () => sum / bets.dollarToRubles;
 
+let eurosToRubles = () => sum * bets.euroToRubles;
+let rublesToEuros = () => sum / bets.euroToRubles;
+
+let CHFsToRubles = () => sum * bets.CHFToRubles;
+let rublesToCHF = () => sum / bets.CHFToRubles;
+
+
+console.log(dollarsToRubles());
+console.log(rublesToDollars());
+
+switch(true) {
+    case ( 
+        sum !== null 
+        && currency !== null 
+        && targetCurrency !== null) :
+        console.log('ok');
+    default: 
+        console.log('Пожалуйста, заполните все запрашиваемые данные');
 }
