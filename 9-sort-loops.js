980d5057866fc1606c96b95ca6261278cc10d887
Khausta <anna.a.khaustova@gmail.com>
2023-09-02 10:49:48 +0300

    sortArr function is added

--- 9-sort-loops.js
+++ 9-sort-loops.js
@@ -1,10 +1,12 @@
 let arr1 = [1, 40, -5, 10, 0];
 let arr2 = [100, 110, -5, -4, 2];
 
+function sortArr(arr, method = 'asc') {
+    method === 'desc' ? decreaseSort(arr) : increaseSort(arr);
+}
 
 function increaseSort(arr) {
     for (let i = 1; i < arr.length; i++) {
-        
         for (let j = 0; j < i; j++) {
             if (arr[i] < arr[j]) {
                 [arr[j], arr[i]] = [arr[i], arr[j]];
@@ -25,12 +27,10 @@ function decreaseSort(arr) {
     console.log(arr);
 }
 
-increaseSort(arr1);
-increaseSort(arr2);
-
-decreaseSort(arr1);
-decreaseSort(arr2);
-
+sortArr(arr1, 'desc');
+sortArr(arr2, 'desc');
+sortArr(arr1, 'asc');
+sortArr(arr2);
 
 
 
