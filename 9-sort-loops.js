b47b0027809eefbe0e8ee793bb2f2a881eb1c22f
Khausta <anna.a.khaustova@gmail.com>
2023-09-03 14:55:03 +0300

    9-sort-loops is modified

--- 9-sort-loops.js
+++ 9-sort-loops.js
@@ -2,41 +2,31 @@ let arr1 = [1, 40, -5, 10, 0];
 let arr2 = [100, 110, -5, -4, 2];
 
 function sortArr(arr, method = 'asc') {
-    if (method === 'asc') {
-        increaseSort(arr);
-    } else if(method === 'desc') {
-        decreaseSort(arr);
-    } else { 
-        console.log('Выберите способ сортировки asc или desc');
-    }
-}
-
-function increaseSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        for (let j = 0; j < i; j++) {
-            if (arr[i] < arr[j]) {
-                [arr[j], arr[i]] = [arr[i], arr[j]];
+    if (method !== 'asc' && method !== 'desc') {
+        console.log('Введен неверный метод сортировки');
+        return;
+    } else {
+        for (let i = 1; i < arr.length; i++) {
+            for (let j = 0; j < i; j++) {
+                switch (method) {
+                    case 'asc':
+                        if (arr[i] < arr[j]) [arr[j], arr[i]] = [arr[i], arr[j]];
+                        break;
+                    case 'desc': 
+                        if (arr[i] > arr[j]) [arr[i], arr[j]] = [arr[j], arr[i]];
+                        break;
+                }
             }
         }
+        console.log(arr);
     }
-    console.log(arr);
-}
-
-function decreaseSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        for (let j = 0; j < i; j++) {
-            if(arr[i] > arr[j]) {
-                [arr[i], arr[j]] = [arr[j], arr[i]];
-            }
-        }
-    }
-    console.log(arr);
 }
 
 sortArr(arr1, 'desc');
 sortArr(arr2, 'desc');
 sortArr(arr1, 'asc');
 sortArr(arr2);
+sortArr(arr2, 'sadjf');
 
 
 
