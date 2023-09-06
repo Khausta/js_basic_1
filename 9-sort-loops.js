7e47c7ee294352e08d4ad03d73276c26c19f1cb1
Khausta <anna.a.khaustova@gmail.com>
2023-09-06 20:57:59 +0300

    9-sort-loops.js modified

--- 9-sort-loops.js
+++ 9-sort-loops.js
@@ -2,24 +2,22 @@ let arr1 = [1, 40, -5, 10, 0];
 let arr2 = [100, 110, -5, -4, 2];
 
 function sortArr(arr, method = 'asc') {
-    if (method !== 'asc' && method !== 'desc') {
-        console.log('Введен неверный метод сортировки');
-        return;
-    } else {
-        for (let i = 1; i < arr.length; i++) {
-            for (let j = 0; j < i; j++) {
-                switch (method) {
-                    case 'asc':
-                        if (arr[i] < arr[j]) [arr[j], arr[i]] = [arr[i], arr[j]];
-                        break;
-                    case 'desc': 
-                        if (arr[i] > arr[j]) [arr[i], arr[j]] = [arr[j], arr[i]];
-                        break;
-                }
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = 0; j < i; j++) {
+            switch (method) {
+                case 'asc':
+                    if (arr[i] < arr[j]) [arr[j], arr[i]] = [arr[i], arr[j]];
+                    break;
+                case 'desc': 
+                    if (arr[i] > arr[j]) [arr[i], arr[j]] = [arr[j], arr[i]];
+                    break;
+                default: 
+                    console.log('Введен неверный метод сортировки');
+                    return;
             }
         }
-        console.log(arr);
     }
+    console.log(arr);
 }
 
 sortArr(arr1, 'desc');
