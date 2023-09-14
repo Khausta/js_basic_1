11453d243703df30f73d7e18ba8a282998b3ef11
Khausta <anna.a.khaustova@gmail.com>
2023-09-14 08:44:21 +0300

    before merge

--- 11-date-array.js
+++ 11-date-array.js
@@ -4,60 +4,58 @@ const correctDateArray = [];
 
 //общая функция проверки и формирования корректного массива
 function checkDates(dateArray) {
-    const myArr = [];
-    const inputArr = dateArray.map(item => item.split(''));
-    rusOrEnglishCheck(inputArr, myArr);
-    dateValidator(myArr);
-    console.log(correctDateArray);
+    const inputArr = dateArray.map(item => transformDateStringToArray(item));
+    console.log(inputArr.length);
+    
+    const correctDates = dateValidator(inputArr);
+    console.log(correctDates);
+   
 }
 
-//функция проверки формата даты
-function rusOrEnglishCheck (dateArray, newArray) {
-    dateArray.reduce((acc, date) => {
-        acc = date;
-        const symbol = acc.find(el => el === '-' || el === '/');
-        switch(symbol) {
-            case '-':
-                newArray.push(acc.join(''));
-                break;
-            case '/':
-                acc = fromEngToRus(date);
-                newArray.push(acc);
-                break;
-            default: 
-                return;
-        }
-    }, 0);
-}
 
-//функция перевода английского формата в русский
-function fromEngToRus(date) {
-    date = date.join('').split('/');
-    let day = date[1];
-    let month = date[0];
-    let year = date[2];
-    date = [day, month, year].join('-');
-    return date;
-}
+function transformDateStringToArray(dateString) {
+    let [month, day, year] = dateString.split('/');
+    console.log([month, day, year]);
 
-//функция проверки дат русского формата и переведенных в русский формат
-function dateValidator(checkedDateArr) {
-    for (const date of checkedDateArr) {
-        const item = date.split('-');
-        if(+item[0] > 0 &&  +item[0] < 32) {
-            if (+item[1] > 0 &&  +item[1] < 13) {
-                if(+item[2] > 1990 &&  +item[2] < 2024) {
-                    correctDateArray.push(date);
-                } else {
-                    continue;
-                }
-            } else {
-                continue;
-            }
-        } else {
-            continue;
-        }
+    if (!year) {
+       [day, month, year] = dateString.split('-');
+       console.log([day, month, year]);
     }
+
+    if(!year || isNaN(day) || isNaN(month) || isNaN(year)) {
+        return null
+    }
+
+    return [day, month, year];  
+}
+
+//функция валидации
+function dateValidator(dateArray) {
+    console.log(dateArray)
+    const validatedDatesArray = [];
+
+    for (const date of dateArray) {
+        console.log(date)
+        if(date === null) continue;
+        const [day, month, year] = date;
+        console.log(day, month, year);
+
+        if(month < 1 || month > 12) {
+            console.log(month)
+            console.log(month < 1 || month > 12)
+            return
+        } else if (day < 1|| day > 31) {
+            return  
+        } else if (month === 4 && day > 28) {
+            return
+        } else if (year < 1) {
+            return
+        }
+        console.log([day, month, year].join('-'));
+        validatedDatesArray.push([day, month, year].join('-'));
+    }
+    console.log(validatedDatesArray);
+    return validatedDatesArray;
 }
 
 checkDates(arrayOfDates);
